db4b6b6f60051f157cf23ca09d427f6d7c291349
Neha Kumari <nehagupta3136@gmail.com>
2021-05-20 15:57:34 +0530

    springboot program

---
+++ src/test/java/com/springn/H.java
@@ -0,0 +1,8 @@
+package com.springn;
+
+public class H {
+    public static void main(String[] args) {
+        char ch[] = {'e','l','e','p','h','a','n','t'};
+        System.out.println(ch);
+    }
+}
